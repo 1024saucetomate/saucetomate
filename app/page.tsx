03b9a02fd365a4a1580b116580585b37b471c5a4
Yaniv <120222659+y4aniv@users.noreply.github.com>
2024-10-04 22:12:26 +0000

    Update root page header text

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,14 @@
-"use client";
-
 import MatterScene from "@/components/MatterScene";
 import styles from "@/styles/app/root.module.css";
 
 export default function Root() {
   return (
     <>
+      <h3 className={styles.header}>{"SAUCETOMATE"}</h3>
       <div className={styles.container}>
         <MatterScene className={styles.container__matterScene} />
         <div className={styles.container__menu}>
-          <h1>{"Démarrer maintenant"}</h1>
+          <h1>{"Démarrer"}</h1>
           <h3>{"Les programmes"}</h3>
           <h3>{"À propos"}</h3>
         </div>
