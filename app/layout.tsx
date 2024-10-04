b490ba243d0723814de0afbf1c8229c6bcaf9caa
Yaniv <120222659+y4aniv@users.noreply.github.com>
2024-10-04 12:48:50 +0000

    Add empty layout and page components

---
+++ app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+import "@/styles/globals.css";
+
+export const metadata: Metadata = {
+  title: "SAUCETOMATE",
+  description: "Et si vous deviez voter ?",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="fr">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
