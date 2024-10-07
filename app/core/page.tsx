fdeb17c4ae7e9d8364145bb96b0e611df7635473
Yaniv <yanivdouieb@icloud.com>
2024-10-07 21:44:24 +0200

    Refactor Swipper component to support dynamic card data and track remaining cards

--- app/core/page.tsx
+++ app/core/page.tsx
@@ -1,7 +1,10 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
+
+import SlotCounter from "react-slot-counter";
 import Link from "@/components/Link";
+import MatterEndingScene from "@/components/MatterEndingScene";
 import Swipper from "@/components/Swipper";
 import coreDataImport from "@/data/core.json";
 import styles from "@/styles/app/core.module.css";
@@ -32,13 +35,15 @@ type RandomPolicyType = PolicyType & { candidate: string };
 export default function Core() {
   const [randomPolicies, setRandomPolicies] = useState<RandomPolicyType[]>([]);
   const [cardsRemaining, setCardsRemaining] = useState<number>(0);
+  const [bestCandidate, setBestCandidate] = useState<string>("");
+  const [showEndingScene, setShowEndingScene] = useState<boolean>(false);
 
   useEffect(() => {
     const newRandomPolicies: RandomPolicyType[] = [];
     Object.entries(coreData).forEach(([candidate, candidateData]) => {
       if (Array.isArray(candidateData.policies)) {
         const randomCandidatePolicies = candidateData.policies
-          .toSorted(() => Math.random() - 0.5)
+          .sort(() => Math.random() - 0.5)
           .slice(0, 10)
           .map((policy) => ({
             ...policy,
@@ -53,7 +58,7 @@ export default function Core() {
       const randomCandidate =
         candidates[Math.floor(Math.random() * candidates.length)];
       const extraPolicy = coreData[randomCandidate].policies
-        .toSorted(() => Math.random() - 0.5)
+        .sort(() => Math.random() - 0.5)
         .slice(0, 1)
         .map((policy) => ({
           ...policy,
@@ -62,31 +67,63 @@ export default function Core() {
       newRandomPolicies.push(...extraPolicy);
     }
 
-    setRandomPolicies(newRandomPolicies.toSorted(() => Math.random() - 0.5));
+    setRandomPolicies(newRandomPolicies.sort(() => Math.random() - 0.5));
   }, []);
 
   useEffect(() => {
     setCardsRemaining(randomPolicies.length);
   }, [randomPolicies]);
 
+  useEffect(() => {
+    if (cardsRemaining === 0 && bestCandidate) {
+      // Delay the start of the ending scene
+      const timer = setTimeout(() => {
+        setShowEndingScene(true);
+      }, 1000); // Adjust this delay as needed
+
+      return () => clearTimeout(timer);
+    }
+  }, [cardsRemaining, bestCandidate]);
+
   return (
-    <div className={styles.container}>
-      <Link href={"/"} className={styles.header}>
-        <h3>{"Retour"}</h3>
-      </Link>
-      <div className={styles.container__swipper}>
-        <small className={styles.container__swipper_remaining}>
-          {`${cardsRemaining === 0 ? "Aucune" : cardsRemaining.toString()} ${
-            cardsRemaining === 1 ? "proposition" : "propositions"
-          } restante${cardsRemaining === 1 ? "" : "s"}`}
-        </small>
-        <Swipper
-          policies={randomPolicies}
-          className={styles.container__swipper_app}
-          cardsRemaining={cardsRemaining}
-          setCardsRemaining={setCardsRemaining}
-        />
+    <>
+      <div className={styles.container}>
+        <div className={styles.container__header}>
+          <h3>
+            <Link href={"/"} className={styles.container__header_link}>
+              {"Retour"}
+            </Link>
+          </h3>
+          <span className={styles.container__header_counter}>
+            <SlotCounter
+              value={Math.round(
+                ((randomPolicies.length - cardsRemaining) /
+                  randomPolicies.length) *
+                  100,
+              )}
+              duration={0.5}
+              charClassName={styles.container__header_counter_char}
+            />
+            <span>%</span>
+          </span>
+        </div>
+        <div className={styles.container__swipper}>
+          <Swipper
+            policies={randomPolicies}
+            className={styles.container__swipper_app}
+            cardsRemaining={cardsRemaining}
+            setCardsRemaining={setCardsRemaining}
+            setBestCandidate={setBestCandidate}
+          />
+        </div>
       </div>
-    </div>
+      {showEndingScene && (
+        <MatterEndingScene
+          className={styles.matter_scene}
+          candidate={bestCandidate as "Donald Trump" | "Kamala Harris"}
+          start={true}
+        />
+      )}
+    </>
   );
 }
