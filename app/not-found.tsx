772124672bad87c481e3963f15f512d76bc5c91a
Yaniv <yanivdouieb@icloud.com>
2024-10-08 23:15:17 +0200

    Add NotFound component

---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+import styles from "@/styles/app/notfound.module.css";
+
+export default function NotFound() {
+  return (
+    <div className={styles.container}>
+      <h1>404</h1>
+    </div>
+  );
+}
