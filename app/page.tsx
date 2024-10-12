e6c10ea9b324aac287a9fb48f28c0a2beb21cf12
Yaniv <yanivdouieb@icloud.com>
2024-10-12 16:50:49 +0200

    Add menu and CandidatesBubble to root page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,25 @@
+import Link from "@/components/Link";
+import Matter from "@/components/Matter";
+import styles from "@/styles/app/root.module.css";
+
 export default function Root() {
-  return <></>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.container__menu}>
+        <h1>SAUCETOMATE</h1>
+        <div>
+          <Link href="#">
+            <h3>Démarrer</h3>
+          </Link>
+          <Link href="#">
+            <h3>Les programmes</h3>
+          </Link>
+          <Link href="#">
+            <h3>À propos</h3>
+          </Link>
+        </div>
+      </div>
+      <Matter.CandidatesBubble className={styles.container__scene} />
+    </div>
+  );
 }
