69eacb357970dddb7deaa3143630f230266e2de0
Yaniv <yanivdouieb@icloud.com>
2024-10-13 19:04:06 +0200

    Create Swipe page with header, percentage counter & CardStack component

---
+++ app/swipe/page.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useState } from "react";
+import SlotCounter from "react-slot-counter";
+
+import CardStack from "@/components/CardStack";
+import Link from "@/components/Link";
+import styles from "@/styles/app/swipe.module.css";
+
+export default function Swipe() {
+  const [percentage, setPercentage] = useState(0);
+
+  function handlePercentageUpdate(percentage: number) {
+    setPercentage(percentage);
+  }
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <Link href="/">
+          <h3>Retour</h3>
+        </Link>
+        <h3 className={styles.header__percentage}>
+          <SlotCounter value={Math.round(percentage) || 0} duration={0.5} /> <span>%</span>
+        </h3>
+      </div>
+      <div className={styles.stack__container}>
+        <CardStack className={styles.stack} onPercentageUpdate={handlePercentageUpdate} />
+      </div>
+    </div>
+  );
+}
