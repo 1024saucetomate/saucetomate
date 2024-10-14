13a5a7c0aed4ca72ee4125e064bd0cbe1f027e4b
Yaniv <120222659+y4aniv@users.noreply.github.com>
2024-10-14 21:12:46 +0000

    Update MockAPI to include a new method to get a candidate by ID

--- utils/MockAPI/index.ts
+++ utils/MockAPI/index.ts
@@ -1,9 +1,12 @@
-import { all as getCandidates } from "./methods/get/candidates";
+import { all as getCandidates, fromId as getCaniddateFromId } from "./methods/get/candidates";
 import { fromId as getPolicyFromId, random as getRandomPolicies } from "./methods/get/policies";
 
 export default class MockAPI {
   static get = {
-    candidates: getCandidates,
+    candidates: {
+      all: getCandidates,
+      id: getCaniddateFromId,
+    },
     policies: {
       random: getRandomPolicies,
       id: getPolicyFromId,
--- utils/MockAPI/methods/get/candidates.ts
+++ utils/MockAPI/methods/get/candidates.ts
@@ -3,3 +3,7 @@ import candidates from "@/data/candidates.json";
 export function all() {
   return candidates;
 }
+
+export function fromId(id: string) {
+  return candidates.find((candidate) => candidate.id === id);
+}
