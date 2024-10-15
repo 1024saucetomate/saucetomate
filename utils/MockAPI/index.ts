d737641d561bb891eb40e8a7d903d59804c8e3d9
Yaniv <120222659+y4aniv@users.noreply.github.com>
2024-10-15 20:32:16 +0000

    Fix typo in method name for getting a candidate by ID

--- utils/MockAPI/index.ts
+++ utils/MockAPI/index.ts
@@ -1,11 +1,11 @@
-import { all as getCandidates, fromId as getCaniddateFromId } from "./methods/get/candidates";
+import { all as getCandidates, fromId as getCandidateFromId } from "./methods/get/candidates";
 import { fromId as getPolicyFromId, random as getRandomPolicies } from "./methods/get/policies";
 
 export default class MockAPI {
   static get = {
     candidates: {
       all: getCandidates,
-      id: getCaniddateFromId,
+      id: getCandidateFromId,
     },
     policies: {
       random: getRandomPolicies,
