b13fcdd6cc1e901c2681d45e5efed68f0a6fe9c9
Yaniv <yanivdouieb@icloud.com>
2024-10-31 15:19:04 +0100

    Add middleware for vote ID validation in API requests

---
+++ middleware.ts
@@ -0,0 +1,14 @@
+import axios from "axios";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname.startsWith("/vote/")) {
+    const voteId = request.nextUrl.pathname.split("/")[2];
+    try {
+      await axios.get(`${request.nextUrl.origin}/api/vote/${voteId}`);
+      return NextResponse.next();
+    } catch {
+      return NextResponse.error();
+    }
+  }
+}
